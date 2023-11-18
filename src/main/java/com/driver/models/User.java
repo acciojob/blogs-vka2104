bfb6a5c151f07d2ecf8ab3a359d32f421e93f460
anjana <anjana123@gmail.com>
2023-11-18 21:43:56 +0530

    testing issue fix 1

--- src/main/java/com/driver/models/User.java
+++ src/main/java/com/driver/models/User.java
@@ -8,7 +8,6 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
-    @Column(unique = true, nullable = false)
     private String userName;
     @Column(nullable = false)
     private String Password;
